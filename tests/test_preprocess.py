93aa54afcb4d9de937a93495d7f1861aac8df2da
Naoto Kimura <acidyasucherry@gmail.com>
2020-04-13 03:38:00 +0900

    branchミスった

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -2,9 +2,12 @@ import pandas as pd
 
 from package.preprocess import Rename
 
-path = "../input/train_data.csv"
-train = pd.read_csv(path)
+train_path = "../input/train_data.csv"
+train = pd.read_csv(train_path)
+
+
 def test_rename_t():
     pass
+
 def test_rename_p():
     pass
