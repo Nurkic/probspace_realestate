735f09263bbd230b050032823225c3260e25e6eb
Naoto Kimura <acidyasucherry@gmail.com>
2020-04-13 03:07:39 +0900

    branchミスった

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,5 +1,9 @@
-from package.preprocess import _Rename
+import pandas as pd
 
+from package.preprocess import Rename
+
+path = "../input/train_data.csv"
+train = pd.read_csv(path)
 def test_rename_t():
     pass
 def test_rename_p():
