9d46dbfc52b6b8d17395a743b23432000a85d2f5
Naoto Kimura <acidyasucherry@gmail.com>
2020-04-25 18:11:26 +0900

    Add impute.py

---
+++ package/impute.py
@@ -0,0 +1,36 @@
+""" For missing values imputation"""
+
+import numpy as np
+import pandas as pd
+
+
+class Imputer:
+    def __init__(
+        self,
+        df: pd.Dataframe
+    ) -> None:
+        self.df = df
+
+    
+    def num_imputer(
+        self, 
+        cols: list
+    ) -> pd.DataFrame:
+        df = self.df.copy()
+        for col in cols:
+            df[col] == df[col].fillna(df[col].mean())
+        
+        return df
+
+
+    def cat_imputer(
+        self,
+        cols: list
+    ) -> pd.DataFrame:
+        df = self.df.copy()
+        for col in cols:
+            df[col] == df[col].fillna(df[col].mode())
+        
+        return df
+
+
